f845983989787bcfa533dc0ca1d52310a64ecda8
Tushar Barge <tusharbarge433@gmail.com>
2026-04-06 12:49:27 +0530

    Update welcome message in Application class

--- com.resort.solution/src/main/java/com/resort/solution/Application.java
+++ com.resort.solution/src/main/java/com/resort/solution/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("\n\n\t\tHey Zatu, your server is started successfully. Let's enjoy 👾👾👾🤖🤖🤖...");
+		System.out.println("\n\n\t\tHey Tushar, your server is started successfully. Let's enjoy 👾👾👾🤖🤖🤖...");
 	}
 
 }
